967c0364fa3bf4e5c060b492e06a96ba99ff3144
Alberto Pirovano <alberto.pirovano@onfido.com>
2022-10-19 19:46:07 +0100

    fix to plots

---
+++ build/static/js/17.f04a9116.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpamplify-app"]=this["webpackJsonpamplify-app"]||[]).push([[17],{223:function(t,n,e){"use strict";e.r(n),e.d(n,"amplify_auth_container",(function(){return p}));var i=e(14),p=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("form",{autoComplete:"on",hidden:!0},Object(i.i)("input",{name:"username"}),Object(i.i)("input",{name:"password",type:"password"}),Object(i.i)("input",{type:"submit"})),Object(i.i)("slot",null))},t}()}}]);
+//# sourceMappingURL=17.f04a9116.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './App.scss';
+
 import { Component } from 'react';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
 import { listTrips } from './graphql/queries';
@@ -13,28 +15,20 @@ Chart.register(CategoryScale);
 
 Amplify.configure(awsExports);
 
-class LineChart extends Component {
+class LiveDataVisualizer extends Component {
+
   state = {
-    data: {
-        labels: ['0'],
-        datasets: [
-          {
-            label: 'Test',
-            fill: false,
-            lineTension: 0.5,
-            backgroundColor: 'rgba(75,192,192,1)',
-            borderColor: 'rgba(0,0,0,1)',
-            borderWidth: 2,
-            data: [0]
-          }
-        ]
-      }
+    trips: [],
+    plot1Data: {
+        labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+        datasets: []
+    }
   }
 
   componentDidMount() {
     this.timer = setInterval(
       () => this.fetch(),
-      5000
+      1000
     )
   }
 
@@ -42,34 +36,86 @@ class LineChart extends Component {
     clearInterval(this.timer)
   }
 
-  fetch() {
-      const datasetsCopy = this.state.data.datasets.slice(0);
-      const dataCopy = datasetsCopy[0].data.slice(0);
-      const labelsCopy = this.state.data.labels;
+  async fetch() {
+      const results = await API.graphql(graphqlOperation(listTrips));
 
-      console.log(dataCopy)
-      console.log(labelsCopy)
+      const fetchedTrips = results.data.listTrips.items;
 
-      const newValue = Math.random() * (100 - 0) + 0;
+      var hours_datasets = {};
+      var day_datasets = {};
+      for (let i=0; i < fetchedTrips.length; i++){
+        const trip = fetchedTrips[i];
+        const date = new Date(trip["startTimestamp"]);
+        const username = trip["username"];
+        const hour = date.getHours();
+        const day = date.getDay();
 
-      labelsCopy.push(String(newValue));
-      dataCopy.push(newValue);
+        if (hours_datasets[username] === undefined){
+            hours_datasets[username] = {};
+        }
+        if (day_datasets[username] === undefined){
+            day_datasets[username] = {};
+        }
+        if (hours_datasets[username][hour] === undefined){
+            hours_datasets[username][hour] = [];
+        }
+        if (day_datasets[username][day] === undefined){
+            day_datasets[username][day] = [];
+        }
 
-      datasetsCopy[0].data = dataCopy;
+        hours_datasets[username][hour].push(parseInt(trip["numberOfUnlocks"]));
+        day_datasets[username][day].push(parseInt(trip["numberOfUnlocks"]));
+      }
+
+      const hours_datasets_parsed = [];
+      for (const [key11, value11] of Object.entries(hours_datasets)) {
+          const sub_dataset1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+          for (const [key12, value12] of Object.entries(value11)) {
+              var sum = 0;
+              for (let j = 0; j < value12.length; j += 1) {
+                sum+=value12[j];
+              }
+              sub_dataset1[key12] = sum/value12.length;
+          }
+          const full_dataset1 = {
+            label: key11,
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: sub_dataset1
+          }
+          hours_datasets_parsed.push(full_dataset1);
+      }
+      console.log(hours_datasets_parsed);
+
+      const plot1LabelsPointer = this.state.plot1Data.labels;
+      plot1LabelsPointer[0] = 0;
 
       this.setState({
-          data: Object.assign({}, this.state.data, {
-              labels: labelsCopy,
-              datasets: datasetsCopy
-          })
+        plot1Data: Object.assign({}, this.state.plot1Data, {
+             labels: plot1LabelsPointer,
+             datasets: hours_datasets_parsed
+        })
       });
+
+      this.setState({ trips: fetchedTrips });
   }
 
   render(){
     return(
-      <div>
-      <Line data = {this.state.data}/>
-      </div>
+        <div>
+            <h1>Dashboard</h1>
+            <h3>By Hour of day</h3>
+            <div>
+                <Line data={this.state.plot1Data}/>
+            </div>
+            <h1>Latest Trips</h1>
+            <div>
+                <TripsList trips={this.state.trips} />
+            </div>
+        </div>
     )
   }
 }
@@ -77,46 +123,42 @@ class LineChart extends Component {
 class TripsList extends Component {
   render() {
     return (
-      <div>
-        {this.props.trips.map(trip =>
-          <div key={trip.id} style={styles.trip}>
-            <p>{trip.id}</p>
-          </div>
-        )}
+      <div className="mdc-data-table" style={styles.trip}>
+        <div className="mdc-data-table__table-container">
+          <table className="mdc-data-table__table" aria-label="Trip">
+            <thead>
+              <tr className="mdc-data-table__header-row">
+                <th className="mdc-data-table__header-cell" role="columnheader" scope="col">TripId</th>
+                <th className="mdc-data-table__header-cell" role="columnheader" scope="col">Username</th>
+                <th className="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Number of Unlocks</th>
+                <th className="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Max Kms Hour (g)</th>
+                <th className="mdc-data-table__header-cell mdc-data-table__header-cell--numeric" role="columnheader" scope="col">Sum Kms</th>
+              </tr>
+            </thead>
+            <tbody className="mdc-data-table__content">
+            {this.props.trips.map(trip =>
+              <tr key={trip.id} className="mdc-data-table__row">
+                <th className="mdc-data-table__cell" scope="row">{trip.id}</th>
+                <th className="mdc-data-table__cell" scope="row">{trip.username}</th>
+                <td className="mdc-data-table__cell mdc-data-table__cell--numeric">{trip.numberOfUnlocks}</td>
+                <td className="mdc-data-table__cell mdc-data-table__cell--numeric">{trip.maxKmsHour}</td>
+                <td className="mdc-data-table__cell mdc-data-table__cell--numeric">{trip.sumKms}</td>
+              </tr>
+            )}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        trips: []
-    };
-  }
-
-  componentDidMount() {
-    this.loadNewData()
-    this.interval = setInterval(() => this.loadNewData(), 1000);
-  }
-
-  async loadNewData() {
-      console.log("checking if there is new data !")
-      var result = await API.graphql(graphqlOperation(listTrips));
-      this.setState({ trips: result.data.listTrips.items });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
 
   render() {
     return (
       <div style={styles.container}>
-        <h1>Trips</h1>
-        <LineChart />
-        <TripsList trips={this.state.trips} />
+        <LiveDataVisualizer />
         <AmplifySignOut />
       </div>
     );
