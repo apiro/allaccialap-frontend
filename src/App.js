add586d1de0f5700539a503900313f96cd3949be
Alberto Pirovano <alberto.pirovano@onfido.com>
2022-10-19 15:28:58 +0100

    dynamic plot

--- src/App.js
+++ src/App.js
@@ -5,7 +5,74 @@ import { listTrips } from './graphql/queries';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import awsExports from './aws-exports';
 
+import Chart from 'chart.js/auto';
+import {CategoryScale} from 'chart.js';
+import {Line} from 'react-chartjs-2';
+
+Chart.register(CategoryScale);
+
 Amplify.configure(awsExports);
+
+class LineChart extends Component {
+  state = {
+    data: {
+        labels: ['0'],
+        datasets: [
+          {
+            label: 'Test',
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: [0]
+          }
+        ]
+      }
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(
+      () => this.fetch(),
+      5000
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer)
+  }
+
+  fetch() {
+      const datasetsCopy = this.state.data.datasets.slice(0);
+      const dataCopy = datasetsCopy[0].data.slice(0);
+      const labelsCopy = this.state.data.labels;
+
+      console.log(dataCopy)
+      console.log(labelsCopy)
+
+      const newValue = Math.random() * (100 - 0) + 0;
+
+      labelsCopy.push(String(newValue));
+      dataCopy.push(newValue);
+
+      datasetsCopy[0].data = dataCopy;
+
+      this.setState({
+          data: Object.assign({}, this.state.data, {
+              labels: labelsCopy,
+              datasets: datasetsCopy
+          })
+      });
+  }
+
+  render(){
+    return(
+      <div>
+      <Line data = {this.state.data}/>
+      </div>
+    )
+  }
+}
 
 class TripsList extends Component {
   render() {
@@ -24,7 +91,9 @@ class TripsList extends Component {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { trips: [] };
+    this.state = {
+        trips: []
+    };
   }
 
   componentDidMount() {
@@ -46,6 +115,7 @@ class App extends Component {
     return (
       <div style={styles.container}>
         <h1>Trips</h1>
+        <LineChart />
         <TripsList trips={this.state.trips} />
         <AmplifySignOut />
       </div>
@@ -56,6 +126,6 @@ class App extends Component {
 export default withAuthenticator(App);
 
 const styles = {
-  container: { margin: '0 auto', padding: 20 },
+  container: {  width: 1000, margin: '0 auto', padding: 20 },
   trip: { display: 'flex', justifyContent: 'space-between', alignItems: 'center', fontSize: 22, marginBottom: 15 },
 }
